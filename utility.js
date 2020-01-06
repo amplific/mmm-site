d1efe5e2fdab7eec7fff7b180c6b0f6126dd31b1
Adam Lake <794096+amplific@users.noreply.github.com>
2020-01-05 21:37:44 -0800

    Add files via upload

---
+++ utility.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	$(".scroll").click(function(event){
+		event.preventDefault();
+		var full_url = this.href;
+		var parts = full_url.split("#");
+		var trgt = parts[1];
+		var target_offset = $("#"+trgt).offset();
+		var target_top = target_offset.top;
+		$('html, body').animate({scrollTop:target_top}, 500);
+	});
+	
+});
